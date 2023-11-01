1f4c6654e8ed8e85d09d9a7b63aef94754936b87
adsalihac <adsalihac@gmail.com>
2023-11-01 20:58:18 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
 
-export default function App() {
+const App = () => {
+  const [bulbOne, setBulbOne] = useState(false); // Store 
+  const [bulbTwo, setBulbTwo] = useState(false); // Store 
+
+  useEffect(() => {
+    // console.log(bulbOne, "bulbOne");
+    // if (bulbOne === true) {
+    //   setBulbTwo(!bulbTwo);
+    // }
+    console.log("Use Effect 1 called");
+    
+    if(bulbOne) {
+      alert("bulb 1 is on")
+    }
+  }, [bulbOne]);
+
+  React.useEffect(() => {
+   console.log("USE EFFECT 2");
+  }, [bulbTwo]);
+
+  // useEffect(() => {
+  //   // first
+  //   console.log("First");
+
+  //   return () => {
+  //     // second
+  //   console.log("Second");
+  //   }
+  // }, [])
+
+  // 1 - screen open
+  // 2 - return screen
+  // 3 - dependency - state change
+
+  // Examples
+  // API CALLING
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <TouchableOpacity
+        style={{
+          height: 100,
+          width: 100,
+          backgroundColor: "red",
+        }}
+        onPress={() => {
+          setBulbOne(!bulbOne);
+        }}
+      ></TouchableOpacity>
+      <Text>{bulbOne ? "Bulb One ON" : "Bulb One OFF"}</Text>
+      <Text>{bulbTwo ? "Bulb Two ON" : "Bulb Two OFF"}</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+const styles = StyleSheet.create({});
